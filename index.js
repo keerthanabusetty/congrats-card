745f8de0a6145b4ee17f5174a219ae03ec5590fa
root <busetty14keerthana@gmail.com>
2023-07-07 06:14:35 +0000

    congrats-card

---
+++ index.js
@@ -0,0 +1,24 @@
+const element = (
+  // Write your code here.
+  <div className="bg-container">
+    <div>
+      <h1 className="">Congratulations</h1>
+    </div>
+    <div className="card-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        className=""
+      />
+      <h1 className="">Kiran V</h1>
+      <p className="">
+        Vishnu Institute of Computer Education And Technology,Bhimavaram
+      </p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        className=""
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
